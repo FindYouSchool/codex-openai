6be501966d30a5bfe5ae2094138fb2f83178e3df
Juvet <juvettaj@gmail.com>
2023-01-19 15:02:47 +0100

    url to local

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from the server => bot 
 
-  const response = await fetch("https://codex-api-x8vk.onrender.com/", {
+  const response = await fetch('http://localhost:5000/', {
     method:'POST',
     headers: {
       'Content-Type': 'application/json',
